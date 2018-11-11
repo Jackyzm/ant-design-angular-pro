455ce30bba06e9ea61283dd462d0f039277c1ef2
jackyzm <jackyzm@foxmail.com>
2018-11-11 23:33:20 +0800

    add routes

---
+++ src/routes/deshboard/analysis/analysis.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analysis',
+  templateUrl: './analysis.component.html',
+  styleUrls: ['./analysis.component.less']
+})
+export class AnalysisComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
